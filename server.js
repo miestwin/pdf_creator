c881f4b5f980e4612a88860e7429cbf6ab8dd78e
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2017-02-18 18:14:52 +0100

    pyramid of doom

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs');
 var pdf = require('html-pdf');
 var express = require('express');
@@ -10,38 +12,65 @@ app.get('/', (req, res) => {
 });
 
 app.get('/download', (req, res) => {
-    let bootstrap = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css";
-    let data = `<!doctype html><html><head><link rel="stylesheet" href="${bootstrap}"></head>
-                <body>${req.query.content}</body></html>`;
-    let options = {
+
+    const bootstrap = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css";
+    const options = {
         "format": "A4",
         "border": "10mm"
     };
-    
 
-    fs.writeFile('myFile.html', data, err => {
-        if (err) { 
+    const min = 100;
+    const max = 10000;
+
+    const data = `<!doctype html><html><head><link rel="stylesheet" href="${bootstrap}"></head>
+                <body>${req.query.content}</body></html>`;
+
+    const pathToFile = `${__dirname}/temp/${Math.floor(Math.random() * (max - min + 1)) + min}`;
+    const pathToHtmlFile = `${pathToFile}.html`;
+    const pathToPdfFile = `${pathToFile}.pdf`; 
+
+    createHtmlAndConvertToPdf(pathToHtmlFile, pathToPdfFile, data, options, res);
+});
+
+//pyramid of doom
+function createHtmlAndConvertToPdf(pathToHtmlFile, pathToPdfFile, content, options, res) {
+    
+    fs.writeFile(pathToHtmlFile, content, (err) => {
+        if (err) {
             console.log(err);
         } else {
-            let html = fs.readFileSync('myFile.html', 'utf8');
-            pdf.create(html, options).toFile('myFile.pdf', (err, info) => {
-                if (err) { 
+
+            fs.readFile(pathToHtmlFile, 'utf8', (err, data) => {
+                if (err) {
                     console.log(err);
                 } else {
-                    res.download(info.filename, info.filename, err => {
-                        if (err) console.log(err);
-                        fs.unlink(info.filename, err => {
-                            if (err) console.log(err);
-                        });
-                        fs.unlink(__dirname + '/myFile.html', err => {
-                            if (err) console.log(err);
-                        });
+
+                    pdf.create(data,options).toFile(pathToPdfFile, (err, info) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+
+                            res.download(info.filename, info.filename, err => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+
+                                    removeFile(info.filename);
+                                    removeFile(pathToHtmlFile);
+                                }
+                            });
+                        }
                     });
                 }
             });
         }
     });
-});
+
+}
+
+function removeFile(pathToFile) {
+    fs.unlink(pathToFile, err => { if (err) console.log(err); });
+}
 
 var port = process.env.PORT || 8000;
 app.listen(port, () => { console.log(`listening on ${port}`); });
