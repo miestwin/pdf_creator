7c72e5c5f0e6f4e91781cf424cbf95dfaf4b596f
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2017-02-22 00:21:25 +0100

    eliminate hidden input

--- public/js/script.js
+++ public/js/script.js
@@ -5,12 +5,10 @@ function handler () {
     const converter = new showdown.Converter();
     const download = document.getElementById('download');
     const pad = document.getElementById('pad');
-    let markdownHidden = document.getElementById('content');
     let markdownArea = document.getElementById('markdown');
 
     const convertTextAreaToMarkdown = () => {
         markdownArea.innerHTML = converter.makeHtml(pad.value);
-        markdownHidden.value = markdownArea.innerHTML;
     }
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var pdf = require('html-pdf');
+var pdf = require('markdown-pdf');
 var express = require('express');
 var app = express();
 
@@ -21,8 +21,8 @@ app.get('/download', (req, res) => {
             res.status(201);
             res.download(response.path_to_pdf_file, response.path_to_pdf_file, (err) => {
                 if (err) Promise.reject(err);
-                if (removeFile(response.path_to_html_file)) Promise.reject(new Error(`Unable to remove ${response.path_to_html_file}`));
-                if (removeFile(response.path_to_pdf_file)) Promise.reject(new Error(`Unable to remove ${response.path_to_html_file}`));
+                if (removeFile(response.path_to_markdown_file)) Promise.reject(new Error(`Unable to remove ${response.path_to_markdown_file}`));
+                if (removeFile(response.path_to_pdf_file)) Promise.reject(new Error(`Unable to remove ${response.path_to_markdown_file}`));
             });
         })
         .catch(err => {
@@ -33,20 +33,14 @@ app.get('/download', (req, res) => {
 
 function generateData (content) {
     
-    const bootstrap = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css";
     const min = 100;
     const max = 10000;
     const pathToFile = `${__dirname}/temp/${Math.floor(Math.random() * (max - min + 1)) + min}`;
 
     let data = {};
-    data.converter_options = {
-        "format": "A4",
-        "border": "10mm"
-    };
-    
-    data.file_content = `<!doctype html><html><head><link rel="stylesheet" href="${bootstrap}"></head>
-                <body>${content}</body></html>`;
-    data.path_to_html_file = `${pathToFile}.html`;
+    data.path_to_css = __dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+    data.file_content = content;
+    data.path_to_markdown_file = `${pathToFile}.md`;
     data.path_to_pdf_file = `${pathToFile}.pdf`; 
 
     return data;
@@ -54,7 +48,7 @@ function generateData (content) {
 
 function createFile (response) {
     return new Promise((resolve, reject) => {
-        fs.writeFile(response.path_to_html_file, response.file_content, (err) => {
+        fs.writeFile(response.path_to_markdown_file, response.file_content, (err) => {
             if (err) reject(err);
             resolve(response);
         });
@@ -63,7 +57,7 @@ function createFile (response) {
 
 function readFile (response) {
     return new Promise((resolve, reject) => {
-        fs.readFile(response.path_to_html_file, 'utf8', (err, data) => {
+        fs.readFile(response.path_to_markdown_file, 'utf8', (err, data) => {
             if (err) reject(err);
             response.readed_html_file = data;
             resolve(response);
@@ -73,9 +67,7 @@ function readFile (response) {
 
 function createPdf (response) {
     return new Promise((resolve, reject) => {
-        pdf.create(response.readed_html_file, response.converter_options).toFile(response.path_to_pdf_file, (err, data) => {
-            if (err) reject(err);
-            response.path_to_pdf_file = data.filename;
+        pdf({ cssPath: response.path_to_css}).from(response.path_to_markdown_file).to(response.path_to_pdf_file, () => {
             resolve(response);
         });
     });
